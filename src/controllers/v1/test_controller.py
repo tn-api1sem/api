582f468c57a5cc8553272d175fec6098e158e4cc
Matheus Fiebig <macrubig@gmail.com>
2022-09-01 20:25:01 -0300

    dev banco

--- src/controllers/v1/test_controller.py
+++ src/controllers/v1/test_controller.py
@@ -1,15 +1,11 @@
 from fastapi import APIRouter
 
-from src.data.repository.user_repository import UserRepository
-
 router = APIRouter(
     prefix="/api/v1/test",
     tags=["test"],
     responses={404: {"description": "Not found"}},
 )
 
-userRepository = UserRepository()
-
 @router.get("/")
 def test_route():
-    return userRepository.insert(1)
+    return `{"response":"OK"}
