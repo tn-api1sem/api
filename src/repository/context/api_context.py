e3daf41c9adba5e31ec13ab7ee03501436b66e0f
Matheus Fiebig <macrubig@gmail.com>
2022-10-09 12:31:21 -0300

    Merge developfeefas with develop

--- src/repository/context/api_context.py
+++ src/repository/context/api_context.py
@@ -4,11 +4,8 @@ from ..context.json_context import JsonContext
 class ApiContext:
     test_table: JsonContext = JsonContext("test.json")
     user_table: JsonContext = JsonContext("users.json")
-<<<<<<< HEAD
     profile_table: JsonContext = JsonContext("profiles.json")
-=======
     sprints_table: JsonContext = JsonContext("sprint.json")
->>>>>>> developfefas
     
     times_table: JsonContext = JsonContext("teams.json")
 
