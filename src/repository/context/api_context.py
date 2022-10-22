dae247927b5a7062950a7386a40abb40e12a18eb
Matheus Fiebig <macrubig@gmail.com>
2022-10-21 22:11:29 -0300

    Criando arquivos de grupo

--- src/repository/context/api_context.py
+++ src/repository/context/api_context.py
@@ -5,6 +5,7 @@ class ApiContext:
     user_table: JsonContext = JsonContext("users.json")
     profile_table: JsonContext = JsonContext("profiles.json")
     sprints_table: JsonContext = JsonContext("sprint.json")
+    group_table: JsonContext = JsonContext("group.json")
     times_table: JsonContext = JsonContext("teams.json")
 
     def __init__(self) -> None:
---
+++ src/services/group_service.py
@@ -0,0 +1,18 @@
+class GroupService(object):
+    def __init__(self) -> None:
+        pass
+
+    def get(self):
+        return 
+        
+    def get_by_id(self, id:int): 
+        return 
+
+    def create(self):
+        return
+
+    def update(self):
+        return;
+
+    def delete(self, id:int):
+        return;
