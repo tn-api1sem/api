b27845bd9415979c97c1bf98d26473d5602992cb
Matheus Fiebig <macrubig@gmail.com>
2022-10-24 14:39:05 -0300

    Front grupos

--- src/web/js/consts.js
+++ src/web/js/consts.js
@@ -1,1 +1,16 @@
 const localURL = "http://127.0.0.1:8000"
+
+
+const formToJson = function (formId){
+    var data = {};
+    $("#"+formId).serializeArray().map( function(x) {
+        data[x.name] = $('#'+x.name).val();
+    });
+    return data;
+}
+
+const jsonToForm = function (formId, json){
+    $("#"+formId).serializeArray().map( function(x) {
+        $('#'+x.name).val(json[x.name]);
+    });
+}
--- src/web/js/teams.js
+++ src/web/js/teams.js
@@ -1,19 +1,3 @@
-let getUsers = async () => {
-    var requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-
-    var users;
-
-    await fetch(localURL + '/api/v1/usuario/', requestOptions)
-        .then(response => response.text())
-        .then(result => users = result)
-        .catch(error => console.log('error', error));
-
-    return JSON.parse(users);
-}
-
 let getTeams = async () => {
     var requestOptions = {
         method: 'GET',
