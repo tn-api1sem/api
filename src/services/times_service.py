e6f3c4e0bc870370c5e8dd033c5dfc559092d8d0
Matheus Fiebig <macrubig@gmail.com>
2022-10-25 20:07:32 -0300

    Desenvolvido front times

--- src/services/times_service.py
+++ src/services/times_service.py
@@ -1,15 +1,25 @@
 from ..models.times_model import times_model
 from ..repository.times_repository import times_repository
+from ..repository.usuario_repository import user_repository
 
 
 class times_services(object):
     _times_repository: times_repository = times_repository()
+    _users_repository: user_repository  = user_repository()
 
     def __init__(self):
         pass
 
     def buscar_times(self):
-        return self._times_repository.get()
+        times = self._times_repository.get();
+
+        for time in times:
+            time.usuarios = []
+            for userId in time.id_users:
+                user = self._users_repository.get_by_id(userId)
+                time.usuarios.append(user.usuario)
+
+        return times
 
     def buscar_id_times(self, id):
         return self._times_repository.busca_id_times(id)
--- src/web/js/consts.js
+++ src/web/js/consts.js
@@ -4,13 +4,31 @@ const localURL = "http://127.0.0.1:8000"
 const formToJson = function (formId){
     var data = {};
     $("#"+formId).serializeArray().map( function(x) {
-        data[x.name] = $('#'+x.name).val();
+        const el = $('#'+x.name).val();
+        
+        let isId = x.name.includes('id');
+        let isArray = Array.isArray(el)
+        
+        if(isArray){
+            for(let i = 0; i < el.length; i++){
+                el[i] = parseInt(el[i]);
+            }
+        }
+        
+        data[x.name] =  isId ? (isArray ? el : parseInt(el)) : el.toString() ;
     });
     return data;
 }
 
-const jsonToForm = function (formId, json){
-    $("#"+formId).serializeArray().map( function(x) {
-        $('#'+x.name).val(json[x.name]);
-    });
+const jsonToForm = function (json){
+    for (const [key, value] of Object.entries(json)) {
+        if(Array.isArray(value)){
+            $('#'+key)
+                .val(value)
+                .change();    
+            continue;
+        }
+            
+        $('#'+key).val(value);
+    }
 }
