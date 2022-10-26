b1059561072f7634a7e3c4b33726f521c6b34c65
zzzidanillo <danillo.silva4@fatec.sp.gov.br>
2022-10-26 07:51:21 -0300

    correcao do parametro rates.json dentro de api_context

--- src/repository/context/api_context.py
+++ src/repository/context/api_context.py
@@ -6,7 +6,7 @@ class ApiContext:
     profile_table: JsonContext = JsonContext("profiles.json")
     sprints_table: JsonContext = JsonContext("sprint.json")
     times_table: JsonContext = JsonContext("teams.json")
-    avaliacao_table: JsonContext = JsonContext("teams.json")
+    avaliacao_table: JsonContext = JsonContext("rates.json")
 
     def __init__(self) -> None:
         pass
