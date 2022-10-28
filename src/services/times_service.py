b1c372288c64ef49658c8b27322b1c140474995f
wdeus <wagner.silva96@fatec.sp.gov.br>
2022-10-27 22:15:44 -0300

    inserção em duas tabelas

--- src/services/times_service.py
+++ src/services/times_service.py
@@ -1,9 +1,11 @@
 from ..models.times_model import times_model
 from ..repository.times_repository import times_repository
+from ..repository.usuario_repository import user_repository
 
 
 class times_services(object):
     _times_repository: times_repository = times_repository()
+    _usuario_repository: user_repository = user_repository()
 
     def __init__(self):
         pass
@@ -22,3 +24,13 @@ class times_services(object):
 
     def delete_id_times(self, id: int):
         return self ._times_repository.delete_id_times(id)
+
+    def create(self, id: times_model):
+        # obtem os id's que foram salvos na team.json
+        listUser = times_model.id_users
+        # percorre a lista
+        for user in listUser:
+            # busca os dados da tabela users
+            usuario = self._usuario_repository.busca_id_usuario(user)
+            # insere
+            self._usuario_repository.post_usuario(usuario)
