ca01a4095a3e96fcd57f9eef9f60e15ef27a2af0
wdeus <wagner.silva96@fatec.sp.gov.br>
2022-10-29 15:37:21 -0300

    Correções

--- src/services/avaliacaoUsuario_service.py
+++ src/services/avaliacaoUsuario_service.py
@@ -1,13 +1,11 @@
 from datetime import datetime
 from ..models.avaliacaoUsuario_model import avaliacaoUsuario_model
 from ..repository.avaliacaoUsuario_repository import avaliacaoUsuario_repository
-from ..repository.avaliacao_repository import avaliacao_repository
 from ..repository.sprint_repository import SprintsRepository
 
 
 class avaliacaoUsuario_service(object):
     _avaliacaoUsuario_repository: avaliacaoUsuario_repository = avaliacaoUsuario_repository()
-    _avaliacao_repository: avaliacao_repository = avaliacao_repository()
     _sprint_repository: SprintsRepository = SprintsRepository()
 
     def __init__(self):
@@ -19,11 +17,15 @@ class avaliacaoUsuario_service(object):
     def buscar_id_avaliacaoUsuario(self, id):
         return self._avaliacaoUsuario_repository.buscar_id_avaliacaoUsuario(id)
 
-    def post_avaliacaoUsuario(self, objectToPost: avaliacaoUsuario_model):
-        return self._avaliacaoUsuario_repository.post_avaliacaoUsuario(objectToPost)
+    def post_avaliacaoUsuario(self, model: avaliacaoUsuario_model):
+        self.validaCadastro(model)
+        self._avaliacaoUsuario_repository.post_avaliacaoUsuario(model)
+        return
 
-    def put_avaliacaoUsuario(self, objectToPut: avaliacaoUsuario_model):
-        return self._avaliacaoUsuario_repository.put_avaliacaoUsuario(objectToPut)
+    def put_avaliacaoUsuario(self, model: avaliacaoUsuario_model):
+        self.validaCadastro(model)
+        self._avaliacaoUsuario_repository.put_avaliacaoUsuario(model)
+        return
 
     def delete_id_avaliacaoUsuario(self, id: int):
         return self._avaliacaoUsuario_repository.delete_id_avaliacaoUsuario(id)
@@ -32,7 +34,7 @@ class avaliacaoUsuario_service(object):
         sprint = self._sprint_repository.get_by_id(
             avaliacaoUsuario_model.sprint_id)
 
-        end_date = datetime.strptime(sprint.end_date, '%Y-%m-%d')
+        end_date = datetime.strptime(sprint.evaluation_range, '%Y-%m-%d')
 
         if end_date < datetime.now:
             raise Exception(
