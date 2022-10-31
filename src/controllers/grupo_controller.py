8d8ba90bb978c0009be1284482cabf532f273d34
wdeus <wagner.silva96@fatec.sp.gov.br>
2022-10-31 19:03:43 -0300

    CRUD (GRUPOS) - OK

--- src/controllers/grupo_controller.py
+++ src/controllers/grupo_controller.py
@@ -10,37 +10,41 @@ router = APIRouter(
     responses={404: {"description": "Not found"}},
 )
 
-grupo_service = GrupoService()
+service = GrupoService()
+
 
 @router.get("/")
 def get_all_grupo():
-    return grupo_service.buscar_grupo()
+    return service.buscar_grupo()
+
 
 @router.get("/{id}")
-def get_grupo_by_id(id:int):
-    return grupo_service.buscar_id_grupo(id);
+def get_grupo_by_id(id: int):
+    return service.buscar_id_grupo(id)
+
 
 @router.post("/")
 def create_grupo(model: grupo_model):
     try:
-        grupo_service.post_grupo(model)
-        return OK
-    except Exception as e:
-        return str(e)
-    
-@router.put("/")
-def update_grupo(model: grupo_model):
-    try:
-        grupo_service.put_grupo(model)
+        service.post_grupo(model)
         return OK
     except Exception as e:
         return str(e)
 
-@router.delete("/{id}")
-def delete_grupo(id:int):
+
+@router.put("/")
+def update_grupo(model: grupo_model):
     try:
-        grupo_service.delete_id_grupo(id)
+        service.put_grupo(model)
         return OK
     except Exception as e:
         return str(e)
-    
+
+
+@router.delete("/{id}")
+def delete_grupo(id: int):
+    try:
+        service.delete_id_grupo(id)
+        return OK
+    except Exception as e:
+        return str(e)
--- src/models/grupo_model.py
+++ src/models/grupo_model.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
 
+
 class grupo_model(BaseModel):
     name: str
     client_id: int
     leader_id: int
-    teams: list [int]
-
+    teams: list[int]
+    id: int
