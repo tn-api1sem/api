67d5ab5cd80f52f752a380c4a708b1499d586682
Matheus Fiebig <macrubig@gmail.com>
2022-10-31 14:18:15 -0300

    Merge conflicts

--- src/repository/context/api_context.py
+++ src/repository/context/api_context.py
@@ -6,14 +6,11 @@ class ApiContext:
     profile_table: JsonContext = JsonContext("profiles.json")
     sprints_table: JsonContext = JsonContext("sprint.json")
     times_table: JsonContext = JsonContext("teams.json")
-<<<<<<< HEAD
     grupo_table: JsonContext = JsonContext("groups.json")
-=======
     userTeam_table: JsonContext = JsonContext("user_team.json")
     avaliacao_table: JsonContext = JsonContext("rates.json")
     avaliacaoUsuario_table: JsonContext = JsonContext("user_rate.json")
 
->>>>>>> develop
 
     def __init__(self) -> None:
         pass
