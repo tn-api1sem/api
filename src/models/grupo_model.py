f1bc1ff4cff82153f36dcabbbac2e617611bb414
Matheus Fiebig <macrubig@gmail.com>
2022-10-31 14:06:45 -0300

    form

--- src/models/grupo_model.py
+++ src/models/grupo_model.py
@@ -3,6 +3,6 @@ from pydantic import BaseModel
 class grupo_model(BaseModel):
     name: str
     client_id: int
-    lider_id: int
-    times: list [int]
+    leader_id: int
+    teams: list [int]
 
