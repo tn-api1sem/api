a9aff7977a8586ceab014cc01adfc9e9bbab6a53
Matheus Fiebig <macrubig@gmail.com>
2022-11-01 22:41:15 -0300

    separação de grupos e times

--- src/models/grupo_model.py
+++ src/models/grupo_model.py
@@ -2,8 +2,15 @@ from pydantic import BaseModel
 
 
 class grupo_model(BaseModel):
-    name: str
-    client_id: int
-    leader_id: int
-    teams: list[int]
-    id: int
+    id: int | None
+    name: str | None
+    client_id: int | None
+    leader_id: int | None
+    teams: list[int] | None
+
+class grupo_bd (BaseModel):
+    id: int | None
+    name: str | None
+    client_id: int | None
+    leader_id: int | None
+    
