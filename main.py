03bb76bcf106340dc393c4f7bc17c3b404ad0d76
ffefas <fernandes03022003@gmail.com>
2022-11-03 23:33:05 -0300

    wip avaliacoes

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*", "null"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
