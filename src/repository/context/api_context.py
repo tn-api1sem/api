a3c3ee600c2af4f20bd5b97526003e252acbcbc3
Matheus Fiebig <macrubig@gmail.com>
2022-11-06 22:32:27 -0300

    Conflitos

--- src/repository/context/api_context.py
+++ src/repository/context/api_context.py
@@ -6,16 +6,11 @@ class ApiContext:
     profile_table: JsonContext = JsonContext("profiles.json")
     sprints_table: JsonContext = JsonContext("sprint.json")
     times_table: JsonContext = JsonContext("teams.json")
-<<<<<<< HEAD
-    avaliacao_table: JsonContext = JsonContext("rates.json")
-    avaliacaoUsuario_table: JsonContext = JsonContext("user_rate.json")
-=======
     grupo_table: JsonContext = JsonContext("groups.json")
     userTeam_table: JsonContext = JsonContext("user_team.json")
     avaliacao_table: JsonContext = JsonContext("rates.json")
     avaliacaoUsuario_table: JsonContext = JsonContext("user_rate.json")
 
->>>>>>> origin/develop
 
     def __init__(self) -> None:
         pass
--- src/services/times_service.py
+++ src/services/times_service.py
@@ -3,25 +3,13 @@ from ..repository.times_repository import times_repository
 from ..repository.userteam_repository import userteam_repository
 from ..repository.usuario_repository import user_repository
 from ..repository.profile_repository import profille_repository
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/develop
 
 class times_services(object):
     _times_repository: times_repository = times_repository()
     _usuario_repository: user_repository = user_repository()
-<<<<<<< HEAD
     _userteam_repository: userteam_repository = userteam_repository()
     _profiles_repository: profille_repository = profille_repository()
     
-=======
-
-    _userteam_repository: userteam_repository = userteam_repository()
-    _profiles_repository: profille_repository = profille_repository()
-    
-
->>>>>>> origin/develop
     def __init__(self):
         pass
 
@@ -67,26 +55,11 @@ class times_services(object):
     def delete_id_times(self, id: int):
         self.delete_id_times(id)
         return self ._times_repository.delete_id_times(id)
-<<<<<<< HEAD
-
-    def create(self, id: times_model):
-        # obtem os id's que foram salvos na team.json
-        listUser = times_model.id_users
-        # percorre a lista
-        for user in listUser:
-            # busca os dados da tabela users
-            usuario = self._usuario_repository.busca_id_usuario(user)
-            # insere
-            self._usuario_repository.post_usuario(usuario)
-=======
         
->>>>>>> origin/develop
     def create_user_team(self,idGroup:int, userTeams:list[user_team_model]):
         for userTeam in userTeams:
             userTeam.id_team = idGroup
             self._userteam_repository.create(userTeam)
-<<<<<<< HEAD
-=======
 
     def update_user_team(self, idTeam:int, userTeams:list[user_team_model]):
         self.delete_id_times(idTeam)
@@ -98,4 +71,3 @@ class times_services(object):
 
         for userProfile in allUserProfiles:
             self._userteam_repository.delete(userProfile.id);
->>>>>>> origin/develop
