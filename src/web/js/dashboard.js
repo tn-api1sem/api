8e89c894b9d5feaf315e4b253611fc599f4f13a3
wdeus <wagner.silva96@fatec.sp.gov.br>
2022-11-16 15:15:49 -0300

    Resolvendo bug do gráfico

--- src/web/js/dashboard.js
+++ src/web/js/dashboard.js
@@ -13,3 +13,19 @@ let getUserTeamByIdUser = async (id) => {
 
     return JSON.parse(userTeams);
 }
+
+let getUserRates = async () => {
+    var requestOptions = {
+        method: 'GET',
+        redirect: 'follow'
+    };
+
+    var teams;
+
+    await fetch(localURL + '/api/v1/avaliacaoUsuario/', requestOptions)
+        .then(response => response.text())
+        .then(result => teams = result)
+        .catch(error => console.log('error', error));
+
+    return JSON.parse(teams);
+}
