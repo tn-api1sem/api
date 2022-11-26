8abeb460b4be816763a1839a17a66b9e76b3c4da
Matheus Fiebig <macrubig@gmail.com>
2022-11-26 14:55:12 -0300

    correção bug avaliação + teams

--- src/models/avaliacaoUsuario_model.py
+++ src/models/avaliacaoUsuario_model.py
@@ -2,9 +2,13 @@ from pydantic import BaseModel
 
 
 class avaliacaoUsuario_model(BaseModel):
-    id: int
+    id: int | None
     rated_user: str | None
     sprint_id: int | None
     rated_by: int | None
-    grade: int | None
+    grade01: int | None
+    grade02: int | None
+    grade03: int | None
+    grade04: int | None
+    grade05: int | None
     comment: str | None
